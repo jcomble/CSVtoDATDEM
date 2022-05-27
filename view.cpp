fc911e3e675d164a122e4c76779181636f18550d
jcomble <jcomble@live.fr>
2022-05-27 17:09:33 +0200

    update range

--- view.cpp
+++ view.cpp
@@ -10,7 +10,7 @@ TableMaker view::get_TableMaker() {
 }
 
 void view::write_ranges(std::vector<Node> vector_nodes, std::ofstream &out_dat_file, std::ofstream &out_dem_file) {
-	int minX = INT_MAX, minY = INT_MAX, maxX = INT_MIN, maxY = INT_MIN;
+	float minX = INT_MAX, minY = INT_MAX, maxX = INT_MIN, maxY = INT_MIN;
 	for (Node tmp_node : vector_nodes) {
 		int tmp_X = tmp_node.get_X();
 		int tmp_Y = tmp_node.get_Y();
@@ -27,13 +27,13 @@ void view::write_ranges(std::vector<Node> vector_nodes, std::ofstream &out_dat_f
 			maxY = tmp_Y;
 		}
 	}
-	int minrangeX, minrangeY, maxrangeX, maxrangeY;
+	float minrangeX, minrangeY, maxrangeX, maxrangeY;
 	if (15 * (maxX - minX) / 100 < 3 ) {
 		minrangeX = minX - 3, maxrangeX = maxX + 3;
 	} else {
-		minrangeX = minX - 15 * (maxX - minX) / 100, maxrangeX = maxX + 15 * (maxX - minX) / 100;
+		minrangeX = minX - 15 * (maxX - minX) / 100., maxrangeX = maxX + 15 * (maxX - minX) / 100.;
 	}
-	if (15 * (maxY - minY) / 100 < 3) {
+	if (15 * (maxY - minY) / 100. < 3) {
 		minrangeY = minY - 3;
 		maxrangeY = maxY + 3;
 	} else {
@@ -44,7 +44,7 @@ void view::write_ranges(std::vector<Node> vector_nodes, std::ofstream &out_dat_f
 	out_dem_file << "set yrange [" << minrangeY << ":" << maxrangeY <<"]" << std::endl;
 }
 
-void view::build(){
+void view::build() {
 	std::vector<Node> vector_nodes = get_TableMaker().get_nodes();
 	std::vector<Connection> vector_connections = get_TableMaker().get_connections();
     std::ofstream out_dat_file;
@@ -56,9 +56,8 @@ void view::build(){
         for (Node tmp_node : vector_nodes) {
         	out_dem_file << "set label \"Node" << tmp_node.get_id() << "\"  at " << tmp_node.get_X() << "," << tmp_node.get_Y() << std::endl;
         }
-
         write_ranges(vector_nodes, out_dat_file, out_dem_file);
-        out_dem_file << "plot \"output_nodes.dat\" every :::0::" << vector_connections.size() - 1<< " with lp, \"\" every :::0::0 with points;" << std::endl;
+        out_dem_file << "plot \"output_nodes.dat\" every :::0::" << vector_connections.size() - 1<< " with lp title \"Aretes\", \"\" every :::0::0 with points title \"Noeuds\";" << std::endl;
         out_dem_file << "pause -1 \" (-> return)\"" << std::endl;
         for (Connection tmp_connection : vector_connections) {
         	Node debut = tmp_connection.get_node_debut();
