fdfc2421c8dab6ca424025abb21d206647a2ea6b
Milou146 <57072450+Milou146@users.noreply.github.com>
2022-05-30 14:13:03 +0200

    clean

--- make.bat
+++ make.bat
@@ -1,4 +1,5 @@
 g++ *.cpp -o execute
 execute.exe test-PMR_simple.csv
 gnuplot output_graph_and_traffics.dem
+del *.exe *.dat *.dem
 pause
--- make.sh
+++ make.sh
@@ -3,3 +3,4 @@ g++ -o csvtodatdem.run *.o -g
 rm *.o
 ./csvtodatdem.run test-PMR_simple.csv 
 gnuplot output_graph_and_traffics.dem
+rm *.run *.dat *.dem
